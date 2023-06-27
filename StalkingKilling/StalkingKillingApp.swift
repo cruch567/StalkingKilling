61a57548034f43a690e0f0a92b3012193de14209
Cami <lexaissupreme@gmail.com>
2023-06-27 09:30:12 -0400

    Initial Commit

---
+++ StalkingKilling/StalkingKillingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StalkingKillingApp.swift
+//  StalkingKilling
+//
+//  Created by scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct StalkingKillingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
