3faf0207030fd8844a699005f7eb2782bbd8c804
Cami <lexaissupreme@gmail.com>
2023-06-28 12:19:19 -0400

    catapult page created and navigation links started

---
+++ StalkingKilling/CannonBall-Page.swift
@@ -0,0 +1,23 @@
+//
+//  CannonBall-Page.swift
+//  StalkingKilling
+//
+//  Created by scholar on 6/28/23.
+//
+
+import SwiftUI
+
+struct CannonBall_Page: View {
+    var body: some View {
+        Image("catapult.jpeg")
+            .resizable()
+            .renderingMode(.original)
+            
+    }
+}
+
+struct CannonBall_Page_Previews: PreviewProvider {
+    static var previews: some View {
+        CannonBall_Page()
+    }
+}
