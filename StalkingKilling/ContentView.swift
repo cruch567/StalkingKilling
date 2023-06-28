352407c683da692251834cb3c834d40a35abf4cb
Maverick <Missmac1126@gmail.coom>
2023-06-28 13:06:37 -0400

    messed with button

--- StalkingKilling/ContentView.swift
+++ StalkingKilling/ContentView.swift
@@ -9,28 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "cloud.fill")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("is the preview working on your side?")
-            /*
-            NavigationLink(destination: ButchersDeath()) {
-                Text("Do Nothing ")
-                    .font(.title3)
-                    .buttonStyle(.borderedProminent)
-                    .cornerRadius(20)
-                    .fontWeight(.medium)
-                    .border(.red)
-                    .foregroundColor(.white)
-                    .background(Color.black)
-             */
-// ABOVE IS NAVIGATION LINK DO NOT DELETE
+        
+        
+        ZStack(alignment: .center){
+            Image("titlescreen.jpeg")
+                .resizable()
+                .padding(.horizontal, -89.0)
+                .edgesIgnoringSafeArea(.all)
+                .frame(width: 700.0, height: 400.0)
+            
+            NavigationLink(destination: Passcode()){
+                Text("Play")
+                }
+                .background(Color(hue: 1.0, saturation: 0.982, brightness: 0.584))
+                .foregroundColor(Color.black)
+                .frame(height: 300.0)
+                .padding(.bottom, -200.0)
+                .font(.largeTitle)
+                .buttonStyle(.bordered)
+                /*
+                 NavigationLink(destination: ButchersDeath()) {
+                 Text("Do Nothing ")
+                 .font(.title3)
+                 .buttonStyle(.borderedProminent)
+                 .cornerRadius(20)
+                 .fontWeight(.medium)
+                 .border(.red)
+                 .foregroundColor(.white)
+                 .background(Color.black)
+                 */
+                // ABOVE IS NAVIGATION LINK DO NOT DELETE
             }
         }
     }
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
+
