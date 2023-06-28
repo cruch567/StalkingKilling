9d61362ed12865f68f25c811a6ab903759948d36
Maverick <Missmac1126@gmail.coom>
2023-06-28 11:52:14 -0400

    button

---
+++ StalkingKilling/Passcode.swift
@@ -0,0 +1,24 @@
+//
+//  Passcode.swift
+//  StalkingKilling
+//
+//  Created by Scholar on 6/28/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct Passcode: View {
+    var body: some View {
+        
+        Text("")
+        
+    }
+}
+
+        
+struct Passcode_Previews: PreviewProvider {
+   static var previews: some View {
+       Passcode()
+    }
+}
--- StalkingKilling/TitleScreen.swift
+++ StalkingKilling/TitleScreen.swift
@@ -9,25 +9,39 @@ import SwiftUI
 
 struct TitlteScreen: View {
     var body: some View {
-        
-        ZStack(alignment: .center){
-            Image("titlescreen.jpeg")
-                .resizable()
-                .padding(.horizontal, -89.0)
-                .edgesIgnoringSafeArea(.all)
-                .frame(width: 700.0, height: 400.0)
+    
+                ZStack(alignment: .center){
+                    Image("titlescreen.jpeg")
+                        .resizable()
+                        .padding(.horizontal, -89.0)
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(width: 700.0, height: 400.0)
+            ZStack{
+                    
+                NavigationLink(destination: Passcode()){
+                    
+                    Button("Play") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        
+                    }
+                    .font(.largeTitle)
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(hue: 1.0, saturation: 0.946, brightness: 0.347))
+                    
+                }
+                
+            }
+            
             
         }
-            
-        
     }
-}
     //fml oh
     
-        struct TitleScreen_Previews: PreviewProvider {
-            static var previews: some View {
-                TitlteScreen()
-            }
+    struct TitleScreen_Previews: PreviewProvider {
+        static var previews: some View {
+            TitlteScreen()
         }
+    }
     
-
+    
+}
