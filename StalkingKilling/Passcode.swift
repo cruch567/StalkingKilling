b4399b0ea852396f5ee0f9bfb78078c4645f8d7a
Maverick <Missmac1126@gmail.coom>
2023-06-28 12:37:50 -0400

    added room

--- StalkingKilling/Passcode.swift
+++ StalkingKilling/Passcode.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct Passcode: View {
     var body: some View {
         
-        Text("")
+        Image("passcoderoom.jpeg")
+        
         
     }
 }
--- StalkingKilling/TitleScreen.swift
+++ StalkingKilling/TitleScreen.swift
@@ -10,30 +10,36 @@ import SwiftUI
 struct TitlteScreen: View {
     var body: some View {
     
-                ZStack(alignment: .center){
-                    Image("titlescreen.jpeg")
-                        .resizable()
-                        .padding(.horizontal, -89.0)
-                        .edgesIgnoringSafeArea(.all)
-                        .frame(width: 700.0, height: 400.0)
-            ZStack{
-                    
+        ZStack(alignment: .center){
+            Image("titlescreen.jpeg")
+                .resizable()
+                .padding(.horizontal, -89.0)
+                .edgesIgnoringSafeArea(.all)
+                .frame(width: 700.0, height: 400.0)
+            
                 NavigationLink(destination: Passcode()){
                     
                     Button("Play") {
                         /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                         
                     }
+                    .background(Color(hue: 1.0, saturation: 0.982, brightness: 0.584))
+                    .foregroundColor(Color.black)
+                    .frame(height: 300.0)
+                    .padding(.bottom, -200.0)
                     .font(.largeTitle)
-                    .buttonStyle(.borderedProminent)
-                    .tint(Color(hue: 1.0, saturation: 0.946, brightness: 0.347))
+                    .buttonStyle(.bordered)
+                    
+                    
+                    
                     
                 }
+               
                 
             }
             
-            
         }
+        
     }
     //fml oh
     
@@ -44,4 +50,4 @@ struct TitlteScreen: View {
     }
     
     
-}
+
