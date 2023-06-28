d8d7003194dc3db32b594d592e0bba4a269f34a8
Cami <lexaissupreme@gmail.com>
2023-06-28 09:05:11 -0400

    here

--- StalkingKilling/CannonBall-Death.swift
+++ StalkingKilling/CannonBall-Death.swift
@@ -1,11 +1,13 @@
 //
-//  ButchersDeath.swift
+//  CannonBall-Death.swift
 //  StalkingKilling
+//
+//  Created by scholar on 6/27/23.
 //
 
 import SwiftUI
 
-struct ButchersDeath: View {
+struct CannonBall_Death: View {
     var body: some View {
         ZStack {
             Color.black
@@ -15,17 +17,17 @@ struct ButchersDeath: View {
                     .font(.largeTitle)
                     .fontWeight(.heavy)
                     .foregroundColor(Color.red)
-                Image("knife.jpeg")
+                Image("blood.jpeg")
                     .renderingMode(.original)
                     .resizable(capInsets: EdgeInsets(top: 5.0, leading: 0.0, bottom: 2.0, trailing: 0.0))
-                    .frame(width: 275, height: 400)
+                    .frame(width: 400, height: 400)
                     .aspectRatio(contentMode: .fit)
                 Text("You have died.")
                     .fontWeight(.medium)
                     .font(.largeTitle)
                     .multilineTextAlignment(.center)
                     .foregroundColor(Color.red)
-                Text("You were killed by The Butcher because your reaction time was too slow.\n\n")
+                Text("You were killed by stupidly cannon-balling yourself into a wall.\n\n")
                     .foregroundColor(Color.white)
                     .font(.title2)
                     .multilineTextAlignment(.center)
@@ -34,8 +36,8 @@ struct ButchersDeath: View {
     }
 }
 
-struct ButchersDeath_Previews: PreviewProvider {
+struct CannonBall_Death_Previews: PreviewProvider {
     static var previews: some View {
-        ButchersDeath()
+        CannonBall_Death()
     }
 }
--- StalkingKilling/ContentView.swift
+++ StalkingKilling/ContentView.swift
@@ -3,22 +3,32 @@
 //  StalkingKilling
 //
 //  Created by scholar on 6/27/23.
-//heyyy
+//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "house.fill")
+            Image(systemName: "cloud.fill")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("is the preview working on your side?")
+            /*
+            NavigationLink(destination: ButchersDeath()) {
+                Text("Do Nothing ")
+                    .font(.title3)
+                    .buttonStyle(.borderedProminent)
+                    .cornerRadius(20)
+                    .fontWeight(.medium)
+                    .border(.red)
+                    .foregroundColor(.white)
+                    .background(Color.black)
+             */
+// ABOVE IS NAVIGATION LINK DO NOT DELETE
+            }
         }
-        .padding() //hah hah made a change
     }
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
