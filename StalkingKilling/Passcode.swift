f2d77ef9154a56ebd9538f7afd3ebb804e8655cf
Maverick <Missmac1126@gmail.coom>
2023-06-28 14:19:40 -0400

    passcode stuff

--- StalkingKilling/Passcode.swift
+++ StalkingKilling/Passcode.swift
@@ -9,18 +9,41 @@ import Foundation
 import SwiftUI
 
 struct Passcode: View {
+    
+    @State private var floor = false
+    @State private var cieling = false
+    @State private var nightstand = false
+    @State private var bed = false
+    
     var body: some View {
         
-        Image("passcoderoom.jpeg")
-        
-        
-        
+        NavigationStack {
+            
+            ZStack (alignment: .bottom){
+                
+                Image("passcoderoom.jpeg")
+                
+                RoundedRectangle(cornerRadius: 10)
+                    .padding(.bottom, 70.0)
+                    .frame(width: 600.0, height: 150.0)
+                
+                Text("*You wake up in a room*")
+                    .font(.title)
+                
+                    .foregroundColor(Color.red)
+                    .padding(.top, 20.0)
+                    .frame(width: 300.0, height: 250.0)
+                    
+                
+            }//end of ZStack
+            
+        }
     }
-}
-
-        
-struct Passcode_Previews: PreviewProvider {
-   static var previews: some View {
-       Passcode()
+    
+    
+    struct Passcode_Previews: PreviewProvider {
+        static var previews: some View {
+            Passcode()
+        }
     }
 }
