9ce6a6214d74b12dcc5f6cbb7d27a45a165024ea
Maverick <Missmac1126@gmail.coom>
2023-06-28 13:10:07 -0400

    hh

--- StalkingKilling/ContentView.swift
+++ StalkingKilling/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
         //k
         //ijibol
         //loko
+        ///jbyiugukg
+        ///klkl
         
         ZStack(alignment: .center){
             Image("titlescreen.jpeg")
